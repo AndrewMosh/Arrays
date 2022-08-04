37c6255ebb4bab32cf0135802a5f8764a937c65a
AndrewMosh <99655206+AndrewMosh@users.noreply.github.com>
2022-08-04 14:28:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-(function () {
+function chooseName () {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
@@ -64,4 +64,4 @@ for (var i = 0; i < names.length; i++) {
   }
 }
 
-})();
+};
