4495de0c9ac6019c784e091a2ca6a51e1a24aadb
AndrewMosh <99655206+AndrewMosh@users.noreply.github.com>
2022-08-04 22:50:33 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+(function () {
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (var i = 0; i < names.length; i++) {
+    var firstAlphabet = names[i].charAt(0).toLowerCase();
+
+  if (firstAlphabet === 'j') {
+    byeSpeaker(names[i]);
+  } else {
+     helloSpeaker(names[i]);
+  }
+}
+    })();
+Footer
