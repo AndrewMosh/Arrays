c0cbdb7efac574785bc4c4e685f7305f803b09f1
AndrewMosh <99655206+AndrewMosh@users.noreply.github.com>
2022-08-04 15:01:02 +0300

    Update speakGoodBye.js

--- speakGoodBye.js
+++ speakGoodBye.js
@@ -9,19 +9,22 @@
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
 // var byeSpeaker =
-var byeSpeaker = {
+(function(window) {
+var byeSpeaker = {};
   
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
-this.speakWord = "Good Bye";
+byeSpeaker.speakWord = "Good Bye";
 
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-speak(name) {
+byeSpeaker.speak=function(name) {
   console.log(this.speakWord + " " + this.name);
 }
 }
+ window.byeSpeaker = byeSpeaker;
+ })(window);
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
 // xxxx.xxxx = byeSpeaker;
