881ab370cbb02cd8727b5490d3407e0c94a868fd
AndrewMosh <99655206+AndrewMosh@users.noreply.github.com>
2022-08-04 14:14:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ for (var i = 0; i < names.length; i++) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (firstLetter === 'j') {
+  if (firstLetter === 'j' || names[i].includes("J")) {
     byeSpeaker.speak(names[i]);
   } else {
     helloSpeaker.speak(names[i]);
