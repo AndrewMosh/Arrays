8280362d6c58e9ebc17397c700b1588d6af16887
AndrewMosh <99655206+AndrewMosh@users.noreply.github.com>
2022-08-04 14:39:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -33,6 +33,8 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
+byeSpeaker.speak(name);
+helloSpeaker.speak(name)
 function chooseName () {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
